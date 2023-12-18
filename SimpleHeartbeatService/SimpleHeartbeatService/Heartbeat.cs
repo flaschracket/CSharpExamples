d7863d5681b12bae1a650a3d2f183f32b433905c
Samsoft <52179120+flaschracket@users.noreply.github.com>
2023-12-18 16:22:28 +0100

    simple Service with Console and topshelf
    example of making service thanks to TimCory
    https://www.youtube.com/watch?v=y64L-3HKuP0&t=12s

---
+++ SimpleHeartbeatService/SimpleHeartbeatService/Heartbeat.cs
@@ -0,0 +1,29 @@
+﻿using System.Timers;
+
+namespace SimpleHeartbeatService
+{public  class Heartbeat
+    {
+        private readonly System.Timers.Timer _timer;
+        public Heartbeat()
+        {
+            _timer = new System.Timers.Timer(1000) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"C:\del\Heartbeat.txt",lines);
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ SimpleHeartbeatService/SimpleHeartbeatService/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SimpleHeartbeatService;
+using Topshelf;
+
+Console.WriteLine("Hello, World!");
+var exitCode = HostFactory.Run(x =>
+    {
+        x.Service<Heartbeat>(s =>
+        {
+            s.ConstructUsing(heartbeat => new Heartbeat());
+            s.WhenStarted(heartbeat => heartbeat.Start());
+            s.WhenStopped(heartbeat => heartbeat.Stop());
+        });
+
+        x.RunAsLocalSystem();
+        x.SetServiceName("HeartbeatService");
+        x.SetDisplayName("Heartbeat Service");
+        x.SetDescription("sample heartbeat service. ");
+    });
+int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+Environment.ExitCode = exitCodeValue;
